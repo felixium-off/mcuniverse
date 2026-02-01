c7fc0e80d56de4e39b276e3857b45258d2dcab97
gclogs <woig9874@naver.com>
2026-02-02 03:39:10 +0900

    Add new dependencies for command framework and entity engine
    - Added `lamp.common` and `lamp.minestom` libraries for handling commands.
    - Included `WorldSeedEntityEngine` for multipart entity functionality.

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,9 @@ repositories {
 dependencies {
     implementation("net.minestom:minestom:2025.12.20-1.21.11")
     implementation("org.slf4j:slf4j-simple:2.0.12")
+    implementation("io.github.revxrsal:lamp.common:4.0.0-rc.14")
+    implementation("io.github.revxrsal:lamp.minestom:4.0.0-rc.14")
+    implementation("net.worldseed.multipart:WorldSeedEntityEngine:11.5.6")
 }
 
 application {
